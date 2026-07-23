e86a427beb2d4968b407ffb309d8845a748d0c83
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-07-23 10:20:55 -0300

    fix(game): reassign allies to living player

--- game/src/scenes/GameScene.ts
+++ game/src/scenes/GameScene.ts
@@ -656,7 +656,7 @@ export class GameScene extends Phaser.Scene {
 
     this.zombieSystem?.update(this.players);
 
-    const leadPlayer = this.players[0];
+    const leadPlayer = this.players.find((player) => !player.isDead()) ?? this.players[0];
     if (leadPlayer) {
       this.allySystem?.update(leadPlayer, this.zombieSystem?.getActiveZombies() ?? [], this.time.now);
     }
